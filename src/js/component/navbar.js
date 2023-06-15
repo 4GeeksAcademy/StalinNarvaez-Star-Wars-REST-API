7cb97fa2b0b6722d0aeee521aae651b151e78b99
Stalin Narváez <108230261+StalinNarvaez@users.noreply.github.com>
2023-06-15 22:52:08 +0000

    alineacion de favoritos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,34 +5,47 @@ import star_Wars_Logo from "../../img/star_Wars_Logo.png";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
-		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/" className="m-2">
-					<img src={star_Wars_Logo} width="70" height="50" />
-				</Link>
-				<div className="dropdown m-2 ">
-					<a className="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
-						Favoritos
-					</a>
-					<ul className="dropdown-menu" style={{width:"200px"}}>
-						{store.favorite.map((item, index) => {
-							return (
-								<li key={index} className="dropdown-item d-flex container">
-									<div className="row">
-    									<div className="col-8 justify-content-start fs-6">
-											<span className="p-2 text-primary">{item}</span>
-										</div>
-										<div className="col-auto justify-content-end">
-											<button className="border border-0" onClick={() => actions.borrarFavoritos(item)}>
-												<i className="fa fa-solid fa-trash"/>
-											</button>
-										</div>
-									</div>	
-								</li>
-							)
-						})}
-					</ul>
-				</div>
-			</nav>
-		);
-};
+  
+	const favoritosCount = store.favorite.length;
+  
+	return (
+	  <nav className="navbar navbar-light bg-light mb-3">
+		<Link to="/" className="m-2">
+		  <img src={star_Wars_Logo} width="70" height="50" />
+		</Link>
+		<div className="dropdown m-2">
+		  <a
+			className="btn btn-secondary dropdown-toggle"
+			href="#"
+			role="button"
+			data-bs-toggle="dropdown"
+			aria-expanded="true"
+		  >
+			Favoritos <span className="badge bg-primary">{favoritosCount}</span>
+		  </a>
+		  <ul className="dropdown-menu" style={{ width: "200px" }}>
+			{store.favorite.map((item, index) => {
+			  return (
+				<li key={index} className="dropdown-item d-flex container">
+				  <div className="row align-items-center">
+					<div className="col-8">
+					  <span className="text-primary">{item}</span>
+					</div>
+					<div className="col-4 text-end justify-content-end">
+					  <button
+						className="border border-0 "
+						onClick={() => actions.borrarFavoritos(item)}
+					  >
+						<i className="fa fa-solid fa-trash" />
+					  </button>
+					</div>
+				  </div>
+				</li>
+			  );
+			})}
+		  </ul>
+		</div>
+	  </nav>
+	);
+  };
+  
